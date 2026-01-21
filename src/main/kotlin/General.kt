9a87278602f0e532d33082a0ace5c7af9421404f
Gong Yilang <23266775@life.hkbu.edu.hk>
2026-01-21 18:16:58 +0800

    Implement the Factory Method Pattern with LordFactory and NonLordFactory

--- src/main/kotlin/General.kt
+++ src/main/kotlin/General.kt
@@ -1,25 +1,42 @@
+import kotlin.math.max
+
 abstract class General {
 //    init {
 //        println("General $name has been created.")
 //    }
     abstract val name: String
-    abstract val maxHP: Int
+    abstract var maxHP: Int
     var currentHP: Int = 0
 }
 
 class LiuBei: General() {
     override val name = "Liu Bei"
-    override val maxHP = 4
+    override var maxHP = 4
 }
 
 class CaoCao: General() {
     override val name = "Cao Cao"
-    override val maxHP = 4
+    override var maxHP = 4
 }
 
 class SunQuan: General() {
     override val name = "Sun Quan"
-    override val maxHP = 4
+    override var maxHP = 4
+}
+
+class ZhangFei: General() {
+    override val name = "Zhang Fei"
+    override var maxHP = 4
+}
+
+class GuanYu: General() {
+    override val name = "Guan Yu"
+    override var maxHP = 4
+}
+
+class ZhaoYun: General() {
+    override val name = "Zhao Yun"
+    override var maxHP = 4
 }
 
 object GeneralManager {
@@ -43,34 +60,63 @@ object GeneralManager {
     }
 }
 
-object GeneralFactory {
-    fun createGeneral(name: String): General {
-        val general =  when(name) {
-            "Liu Bei" -> LiuBei()
-            "Cao Cao" -> CaoCao()
-            "Sun Quan" -> SunQuan()
-            else -> throw IllegalArgumentException("Invalid general name.")
-        }
-        println("General ${general.name} created.")
-        general.currentHP = general.maxHP
-        return general
+abstract class GeneralFactory {
+    abstract fun createRandomGeneral(): General
+
+//    fun createGeneral(name: String): General {
+//        val general =  when(name) {
+//            "Liu Bei" -> LiuBei()
+//            "Cao Cao" -> CaoCao()
+//            "Sun Quan" -> SunQuan()
+//            else -> throw IllegalArgumentException("Invalid general name.")
+//        }
+//        println("General ${general.name} created.")
+//        general.currentHP = general.maxHP
+//        return general
+//    }
+}
+
+class LordFactory: GeneralFactory() {
+    private val listOfGenerals: MutableList<General> = mutableListOf(  // a list of generals not created yet
+        LiuBei(), CaoCao(), SunQuan()
+    )
+
+    override fun createRandomGeneral(): General {
+        val lordGeneral = listOfGenerals.random()
+        listOfGenerals.remove(lordGeneral)
+        lordGeneral.maxHP++
+        lordGeneral.currentHP = lordGeneral.maxHP
+        return lordGeneral
+    }
+}
+
+class NonLordFactory: GeneralFactory() {
+    private val listOfGenerals: MutableList<General> = mutableListOf(
+        ZhangFei(), GuanYu(), ZhaoYun()
+    )
+
+    override fun createRandomGeneral(): General {
+        val nonLordGeneral = listOfGenerals.random()
+        listOfGenerals.remove(nonLordGeneral)
+        nonLordGeneral.currentHP = nonLordGeneral.maxHP
+        return nonLordGeneral
     }
 }
 
 fun main() {
     GeneralManager
-//    val liuBei = LiuBei()
-//    liuBei.currentHP = liuBei.maxHP
-//    val caoCao = CaoCao()
-//    caoCao.currentHP = caoCao.maxHP
-//    val sunQuan = SunQuan()
-//    sunQuan.currentHP = sunQuan.maxHP
-    val liuBei = GeneralFactory.createGeneral("Liu Bei")
-    GeneralManager.addGeneral(liuBei)
-    val caoCao = GeneralFactory.createGeneral("Cao Cao")
-    GeneralManager.addGeneral(caoCao)
-    val sunQuan = GeneralFactory.createGeneral("Sun Quan")
-    GeneralManager.addGeneral(sunQuan)
+    val lordFactory = LordFactory()
+    val nonLordFactory = NonLordFactory()
+
+    for(i in 1..3) {
+        GeneralManager.addGeneral(lordFactory.createRandomGeneral())
+    }
+
+    for(i in 1..3) {
+        GeneralManager.addGeneral(nonLordFactory.createRandomGeneral())
+    }
+
+
     val size = GeneralManager.getGeneralCount()
     println("Total number of generals: $size")
 }
